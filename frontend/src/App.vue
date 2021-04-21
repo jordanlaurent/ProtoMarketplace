a0153a864285077964861a477e58d1e4973979fa
jordanlaurent <laurent.jordan@outlook.fr>
2021-04-21 07:56:32 +0200

    navbar headers

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,8 +5,10 @@
 </template>
 
 <style>
-.color1{ background-color: #1b222e ;}
+.color1{ background-color: whitesmoke ;}
 .color2{background-color: #161616;}
 .colorFont1 {color: white;}
-.colorFont2{color: #8d70c6 ;}
+.colorFont3{color: black;}
+.colorFont2{color: #f5f5f5 ;}
+p,h1,h2,h3,h4,h5,h6,a{font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;}
 </style>
