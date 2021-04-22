58a4bccc0333f849ac28c0860504c74dabed1ef7
jordanlaurent <laurent.jordan@outlook.fr>
2021-04-22 15:43:52 +0200

    Page connexion

--- frontend/src/views/Connexion.vue
+++ frontend/src/views/Connexion.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container centerTxt ">
 	<div class="d-flex justify-content-center h-100">
-		<div class="card">
+		<div class="card colorWTSP">
 			<div class="card-header">
-				<h3>Connexion</h3>
+				<h3>CONNEXION</h3>
 				<div class="d-flex justify-content-end social_icon">
 					<span><i class="fab fa-facebook-square"></i></span>
 					<span><i class="fab fa-google-plus-square"></i></span>
@@ -14,14 +14,18 @@
 				<form>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
+							<span class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  class="bi bi-person " viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+</svg></span>
 						</div>
 						<input type="text" class="form-control" placeholder="Nom d'utilisateur">
 						
 					</div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
+							<span class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+  <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+</svg></span>
 						</div>
 						<input type="password" class="form-control" placeholder="Mots de passe">
 					</div>
@@ -29,7 +33,7 @@
 						<input type="checkbox">Se souvenir de moi
 					</div>
 					<div class="form-group">
-						<router-link to="Acceuil" type="submit" class="btn btn-light float-right login_btn">Connexion</router-link>
+						<router-link to="Acceuil" type="submit" class="btn btn-dark float-right login_btn"> Connexion</router-link>
 					</div>
 				</form>
 			</div>
@@ -66,7 +70,6 @@ height: 370px;
 margin-top: auto;
 margin-bottom: auto;
 width: 400px;
-background-color: rgba(0,0,0,0.5) !important;
 }
 
 .social_icon span{
@@ -92,7 +95,7 @@ top: -45px;
 
 .input-group-prepend span{
 width: 50px;
-background-color: #1b222e;
+background-color: #ffffff;
 color: black;
 border:0 !important;
 }
